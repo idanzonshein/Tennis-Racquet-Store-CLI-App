7519fe3b7c88a9dab050ee269e8a35c0ee9bbc50
Idan <idan.zonshein@powerhrg.com>
2020-05-21 16:51:36 -0400

    Update brand

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,7 @@ require "httparty"
 require "nokogiri"
 require "pry"
 require "require_all"
+require "colorize"
+# require "launchy"
 
 require_all "./lib/"
--- lib/brand.rb
+++ lib/brand.rb
@@ -1,20 +1,29 @@
 class Brand
   attr_reader :name
+  attr_accessor :id
 
   @@all = []
 
   def initialize(name = nil)
     @name = name
     @@all << self
+    @id = @@all.count
   end
 
   def self.all
     @@all
   end
 
-  def self.find_by_name(name)
-    all.find do |brand|
-      brand.name == name
-    end
+  def self.find_by_id(id)
+    all.find { |brand| brand.id == id }
   end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || new(name)
+  end
+
+  def self.find_by_name(name)
+    all.find { |brand| brand.name == name }
+  end
+
 end
