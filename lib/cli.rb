f90375fbbbd10f29e2616d27b97a4ee88e2f499b
Idan <idan.zonshein@powerhrg.com>
2020-05-21 08:19:16 -0400

    Add welcome and scraping to cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,8 +4,33 @@
 # This will have to invoke Scraper
 
 class Cli
-
-  def start
-    puts "Hello World!"
+  def call
+    BrandScraper.new.scrape_brands
+    RacquetScraper.new.scrape_racquets
+    welcome
+    # puts all brands
+    # pick a brand
+    # input = gets.strip
+    # brand = Brand.find_by_name(input)
+    # racquets = Racquet.for_brand(brand) #=> all the racquets for brand X
+    # puts all the racquets
+    # user picks racuet
+    # dig into racquet show page - url
   end
+
+  def welcome
+    puts "Welcome to your virtual tennis racquet assistant!"
+    puts "I will show you available racquets and info based on a specific brand."
+  end
+
+  def list_brands
+
+  end
+
+
+
+
+
+
+
 end
