e8a99584e9a5e3312df7a47e9bf68e8de4f90fa3
Idan <idan.zonshein@powerhrg.com>
2020-05-21 16:51:18 -0400

    Add ability to search individual racquet by name

--- lib/raquet.rb
+++ lib/raquet.rb
@@ -5,21 +5,30 @@
 
 class Racquet
 
-  attr_accessor :name, :price, :rating, :url, :brand
+  attr_accessor :name, :price, :rating, :url, :brand, :info
 
   @@all = []
 
-  def initialize(name = nil, price = nil, rating = nil, url = nil, brand = nil)
+  def initialize(name = nil, price = nil, rating = nil, url = nil, brand = nil, info = nil)
     @name = name
     @price = price
     @rating = rating
     @url = url
     @brand = brand
+    @info = info
     @@all << self
   end
 
   def self.all
     @@all
   end
+
+  def self.select_by_brand(brand)
+    all.select { |raquet| raquet.brand == brand }
+  end
+
+
+
+
 
 end
